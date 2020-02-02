ee16d6d5b486bb62ee95015ad6dc2e3b2992f2ce
Sergey Susev <susev.s.l@sberbank.ru>
2020-02-02 23:00:49 +0500

    Hometask_2

---
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.kotlinexample
+
+/**
+ * @author Susev Sergey
+ */
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun registerUser(
+        fullName: String,
+        email: String,
+        password: String
+    ): User {
+        return User.makeUser(fullName, email = email, password = password)
+            .also { user -> map[user.login] = user }
+    }
+
+    fun loginUser(login: String, password: String): String?{
+        return map[login.trim()]?.run {
+            if (checkPassword(password)) this.userInfo
+            else null
+        }
+    }
+}
